19b2a0e7181498eaa9355c8189a3dd0fe1a7e671
evelynea <evelyneujeneza.com>
2022-06-21 07:09:58 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const selectedItem=document.querySelector('select')
+
+const avatarBtn = document.getElementById('add_new_avatar')
+
+avatarBtn.addEventListener('click',function onOpen(){
+    dialog.showModal()
+})
